be7d751e1a6c6dff10b91fa7f0514d6822061ac0
irfu <irfan@drivetrain.ai>
2024-04-25 18:57:06 +0530

    custom type

--- tap_zohobooks/streams.py
+++ tap_zohobooks/streams.py
@@ -2183,7 +2183,7 @@ class AdvancedAccountTransactionsStream(ZohoBooksStream):
         th.Property("transaction_type", th.StringType),
         th.Property("reference_number", th.StringType),
         th.Property("entity_number", th.StringType),
-        th.Property("debit", th.ObjectType()),
+        th.Property("debit", th.CustomType({"type": ["number", "string"]})),
         th.Property("credit", th.StringType),
         th.Property("net_amount", th.StringType),
         th.Property("contact_id", th.StringType),
