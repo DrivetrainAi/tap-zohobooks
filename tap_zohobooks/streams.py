9668da5c47fbd553dbc464ca243f080581e2945b
Sateesh <78748273+sateesh-dt@users.noreply.github.com>
2024-05-21 18:56:24 +0530

    primary key set as none for account transactions stream.

--- tap_zohobooks/streams.py
+++ tap_zohobooks/streams.py
@@ -1811,7 +1811,7 @@ class EstimatesDetailsStream(ZohoBooksStream):
 class AccountTransactionsStream(ZohoBooksStream):
     name = "account_transactions"
     path = "/chartofaccounts/transactions"
-    primary_keys = ["transaction_id"]
+    primary_keys = None
     records_jsonpath: str = "$.transactions[*]"
     parent_stream_type = ChartOfAccountsStream
 
